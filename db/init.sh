aba7898447808f37e24fa9d034d781c156605fb2
Federico Marcos <marcosfede@gmail.com>
2017-01-15 22:49:34 -0300

    docker should be working now

--- db/init.sh
+++ db/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 psql postgres -h localhost -p 5432 -U postgres -f /code/extensions.sql
-osm2pgsql -d postgres -H localhost -U postgres -P 5432 --hstore --hstore-add-index /code/map.osm
+osm2pgsql -E 3857 --cache-strategy sparse -d postgres -H localhost -U postgres -P 5432 --hstore --hstore-add-index /code/laPlata.osm
 psql postgres -h localhost -p 5432 -U postgres -f /code/ids.sql
