59ab6bf470e2c56319d5c84951a0a309a0c59ca7
Federico Marcos <marcosfede@gmail.com>
2017-01-21 03:01:50 -0300

    using argentina db

--- quecolectivo/api/load.py
+++ quecolectivo/api/load.py
@@ -3,26 +3,29 @@ from django.contrib.gis.utils import LayerMapping
 from .models import WorldBorder
 
 osmpoint_mapping = {
-    'osm_id' : 'osm_id',
-    'name' : 'name',
-    'barrier' : 'barrier',
-    'highway' : 'highway',
-    'ref' : 'ref',
-    'address' : 'address',
-    'is_in' : 'is_in',
-    'place' : 'place',
-    'man_made' : 'man_made',
-    'other_tags' : 'other_tags',
-    'geom' : 'POINT',
+    'osm_id': 'osm_id',
+    'name': 'name',
+    'barrier': 'barrier',
+    'highway': 'highway',
+    'ref': 'ref',
+    'address': 'address',
+    'is_in': 'is_in',
+    'place': 'place',
+    'man_made': 'man_made',
+    'other_tags': 'other_tags',
+    'geom': 'POINT',
 }
 
 world_shp = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), 'data', 'TM_WORLD_BORDERS-0.3.shp'),
+    os.path.join(os.path.dirname(__file__), 'data',
+                 'TM_WORLD_BORDERS-0.3.shp'),
 )
 
-def run(verbose=True):
+
+def run(verbose: bool =True):
     lm = LayerMapping(
         WorldBorder, world_shp, world_mapping,
         transform=False, encoding='iso-8859-1',
     )
     lm.save(strict=True, verbose=verbose)
+
