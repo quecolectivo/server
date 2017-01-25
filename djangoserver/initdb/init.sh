e7a79066ebb477d40a5ffc263f8f9de8f05b55b9
Federico Marcos <marcosfede@gmail.com>
2017-01-24 22:33:12 -0300

    fixed init script

--- djangoserver/initdb/init.sh
+++ djangoserver/initdb/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 psql -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -c "CREATE DATABASE laplata;"
 psql $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/extensions.sql
-osm2pgsql -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/laPlata.osm
+osm2pgsql -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/initdb/laPlata.osm
 psql $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/merge.sql
