38c5f072749a830ef4bf72f9d122d21a2b2c6b9e
Federico Marcos <marcosfede@gmail.com>
2017-01-24 23:29:56 -0300

    added pkey index

--- djangoserver/initdb/merge.sql
+++ djangoserver/initdb/merge.sql
@@ -5,4 +5,5 @@ where route='bus'
 group by osm_id, ref, name 
 order by osm_id;
 
-create index bus_routes_idx on bus_routes using gist(way)
+create index bus_routes_idx on bus_routes using gist(way);
+alter table bus_routes add primary key (osm_id);
