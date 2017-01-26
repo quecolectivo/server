05d1e4c0ee712e7f3bcb3493fbd61d3e042c87d1
Federico Marcos <marcosfede@gmail.com>
2017-01-26 03:57:11 -0300

    installed osm2pgslq from debian sid to avoid name blank bug updated --cache to avoid oom

--- djangoserver/initdb/init.sh
+++ djangoserver/initdb/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 psql postgres -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -c "CREATE DATABASE ${POSTGRES_DATABASE};"
 psql -d $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/extensions.sql
-osm2pgsql -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/initdb/laPlata.osm
+osm2pgsql --cache 100 -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/initdb/laPlata.osm
 psql -d $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/merge.sql
