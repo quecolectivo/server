97851e0d31fd727ca7679c1105eef32000e1ddb9
Federico Marcos <marcosfede@gmail.com>
2017-01-30 04:40:11 -0300

    db downloads on init.sh, uses full argentina .osm

--- djangoserver/initdb/init.sh
+++ djangoserver/initdb/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+curl https://download.geofabrik.de/south-america/argentina-latest.osm.pbf -o initdb/argentina-latest.osm.pbf
 psql postgres -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -c "CREATE DATABASE ${POSTGRES_DATABASE};"
 psql -d $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/extensions.sql
-osm2pgsql --cache 100 -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/initdb/laPlata.osm
+osm2pgsql --cache 100 -E 3857 --slim --cache-strategy sparse -d $POSTGRES_DATABASE -H $POSTGRES_HOST -U $POSTGRES_USER -P 5432 --hstore --hstore-add-index /code/initdb/argentina-latest.osm.pbf
 psql -d $POSTGRES_DATABASE -h $POSTGRES_HOST -p 5432 -U $POSTGRES_USER -f /code/initdb/merge.sql
